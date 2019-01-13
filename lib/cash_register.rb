e4722dce4dde0b0c711aae51e16bf331c54adfaf
github username <github email address>
2019-01-12 21:35:29 -0500

    Test passed.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,40 @@
+require 'pry'
 
+class CashRegister
+
+  attr_accessor :total, :discount, :items
+
+  def initialize(discount=20, total=0)
+    @total = total
+    @discount = discount
+    @items = []
+    #@most_recent_item = most_recent_item
+  end
+
+  def add_item(title, price, quantity=1)
+    @total += (price * quantity)
+    quantity.times do |item|
+      @items << title
+    end
+  end
+
+  def apply_discount
+    percent_off = @discount.to_f/100
+    discount_amt = @total = @total - (@total * percent_off)
+    if discount_amt > 0
+      "After the discount, the total comes to $#{discount_amt.to_i}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def void_last_transaction
+    @total -= @total
+    #bnding.pry
+  end
+    # subtract most recent transaction from @total
+    #last_transaction = @items[-1]
+    #@total -= #@total #last_transaction
+    #binding.pry
+
+end
--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -41,6 +41,7 @@ describe 'CashRegister' do
   describe '#apply_discount' do
     context 'the cash register was initialized with an employee discount' do
       it 'applies the discount to the total price' do
+        #binding.pry
         cash_register_with_discount.add_item("macbook air", 1000)
         cash_register_with_discount.apply_discount
         expect(cash_register_with_discount.total).to eq(800)
